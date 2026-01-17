8279c8f51e033be41b149974257d2755290730da
joeyzou <webrules@gmail.com>
2026-01-17 16:27:39 +0800

    Initial Commit

---
+++ Feedflow/FeedflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedflowApp.swift
+//  Feedflow
+//
+//  Created by Zou, Joey on 2026/1/17.
+//
+
+import SwiftUI
+
+@main
+struct FeedflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
